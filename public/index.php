bd1f6256f2bfde483013e13002323421571235b9
Christian Aldover <cka6@njit.edu>
2018-10-02 18:42:18 -0700

    Fix: updated html::generateTable

--- public/index.php
+++ public/index.php
@@ -36,8 +36,21 @@ class csv {
 class html {
 
     public static function generateTable($records) {
-        $table = $records;
-        return $table;
+        $count = 0;
+        foreach($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
     }
 
 }
