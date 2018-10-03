0fe08c8eeca7ed37c1b9f85ef4b91832ee8c0df9
Christian Aldover <cka6@njit.edu>
2018-10-02 18:33:56 -0700

    Feature: implemented recordFactory class

--- public/index.php
+++ public/index.php
@@ -59,3 +59,12 @@ class record {
         return $array;
     }
 }
+
+class recordFactory {
+
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
+    }
+    
+}
